4b38acc6ffe4eb5c4b11f526ad6b06d2469c991e
grim <75869731+vys69@users.noreply.github.com>
2024-09-16 04:24:58 -0700

    add analytics

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import App from './App.svelte';
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
 
 const app = new App({
 	target: document.body,
-	props: {
-		name: 'world'
-	}
 });
+
+// Initialize Vercel Analytics
+inject({ mode: dev ? 'development' : 'production' });
 
 export default app;
